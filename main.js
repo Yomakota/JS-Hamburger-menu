19f786397fca586af113c374d95020b05f550888
Yomakota <teoperitore@outlook.com>
2021-11-22 17:00:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+
+// Hamburger menu:
+let Menu = document.querySelector('.fa-bars');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// mostrare il menu principale
+Menu.addEventListener('click', function () {
+    hamburgerMenu.style.display = 'block';
+})
+
+let Closedmenu = document.querySelector('.fa-times');
+
+// nascondere il menu principale
+Closedmenu.addEventListener('click', function () {
+    hamburgerMenu.style.display = 'none';
+})
